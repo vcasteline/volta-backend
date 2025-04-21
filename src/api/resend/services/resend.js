ff74e9a3e3075648c6c3f5be1d8c3ff2ad26701e
vcasteline <vcasteline@ufl.edu>
2025-04-20 21:13:35 -0400

    tilde email fix

--- src/api/resend/services/resend.js
+++ src/api/resend/services/resend.js
@@ -258,10 +258,10 @@ module.exports = {
       const spanishWeekdays = {
         'lunes': 1,
         'martes': 2,
-        'miércoles': 3,
+        'miercoles': 3,
         'jueves': 4,
         'viernes': 5,
-        'sábado': 6,
+        'sabado': 6,
         'domingo': 7,
       };
       
